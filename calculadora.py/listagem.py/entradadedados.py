dd87c0f01cd7ff2e264c85b07731ec957a155b82
juniordeveloper11 <juniordeveloperbr@gmail.com>
2022-01-30 22:42:39 -0300

    Create entradadedados.py

---
+++ calculadora.py/listagem.py/entradadedados.py
@@ -0,0 +1,12 @@
+
+
+nome = input (" digite seu nome:")
+print ( "voce digitou %s" % nome )
+print (" ola , %s !" % nome )
+
+nome = input(" digite seu nome")
+idade = int (input( "digite  sua idade:"))
+saldo = float(input(" digite seu saldo de sua conta bancaria:"))
+print (nome)
+print (idade)
+print (saldo)
