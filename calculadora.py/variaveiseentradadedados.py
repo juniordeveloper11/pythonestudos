ec37afb13a9a1dc3a9e37570ee13f2845b537efc
juniordeveloper11 <juniordeveloperbr@gmail.com>
2022-01-30 22:10:34 -0300

    Create variaveiseentradadedados.py

---
+++ calculadora.py/variaveiseentradadedados.py
@@ -0,0 +1,39 @@
+#Tipo Inteiro (int)
+#Ponto Flutuante ou Decimal (float)
+#Complexo (complex)
+#String (str)
+#Boolean (bool)
+#Listas (list)
+#Tuplas (tuple)
+#Dicionários (dict)
+
+idade = 18
+ano = 2002
+
+print(type(idade))
+print(type(ano))
+
+altura = 1.80
+peso = 73.55
+
+print(type(peso))
+print(type(altura))
+
+a = 5+2j
+b = 20+6j
+
+print(type(a))
+print(type(b))
+print(complex(2, 5))
+
+nome = 'kassya'
+profissao = 'corretora de imoveis'
+
+print(type(profissao))
+print(type(nome))
+
+fim_de_semana = True
+feriado = False
+
+print(type(fim_de_semana))
+print(type(feriado))
